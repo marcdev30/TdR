97c0d15d3903e7ba828cfa3c5d1185e9ea377258
marcdev30 <programarc30@gmail.com>
2023-08-06 21:49:58 +0200

    Dissenyar quadrícula menú

--- frontend/src/components/menu/CategoriaProducte.jsx
+++ frontend/src/components/menu/CategoriaProducte.jsx
@@ -1,5 +1,11 @@
-const CategoriaProducte = () => {
-	return <div className="categoria"></div>;
+const CategoriaProducte = ({ imatge, nom }) => {
+	return (
+		<div className="categoria">
+			<img src={imatge} alt="Imatge de la categoria" />
+			<hr />
+			<p className="categoria-nom">{nom}</p>
+		</div>
+	);
 };
 
 export default CategoriaProducte;
--- frontend/src/components/menu/QuadriculaCategories.jsx
+++ frontend/src/components/menu/QuadriculaCategories.jsx
@@ -3,12 +3,14 @@ import CategoriaProducte from "./CategoriaProducte";
 const QuadriculaCategories = () => {
 	return (
 		<div className="quadricula">
-			<CategoriaProducte />
-			<CategoriaProducte />
-			<CategoriaProducte />
-			<CategoriaProducte />
-			<CategoriaProducte />
-			<CategoriaProducte />
+			{[...Array(6)].map(categoria => (
+				<CategoriaProducte
+					imatge={
+						"https://europastry.com/es/wp-content/uploads/sites/5/2021/05/3330_r1_6.jpeg"
+					}
+					nom={"Entrepans"}
+				/>
+			))}
 		</div>
 	);
 };
