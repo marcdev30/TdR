1d31b3261d28ba8c4b27d93cb142954a7df0ac49
marcdev30 <programarc30@gmail.com>
2023-08-06 15:14:23 +0200

    Afegir components

---
+++ frontend/src/components/inici/Presentacio.jsx
@@ -0,0 +1,12 @@
+import Button from "react-bootstrap/Button";
+
+const Presentacio = () => (
+  <div className="presentacio">
+    <h1>Cafeteria Serrallarga</h1>
+    <p>Evita les cues i reserva ara la teva comanda.</p>
+    <Button variant="dark">Veure el menú</Button>
+    <Button variant="outline-light">Iniciar sessió</Button>
+  </div>
+);
+
+export default Presentacio;
--- frontend/src/pages/PaginaPrincipal.js
+++ frontend/src/pages/PaginaPrincipal.js
@@ -1,13 +1,15 @@
 import MesDemanat from "../components/inici/MesDemanat";
 import Qualitats from "../components/inici/Qualitats";
+import Presentacio from "../components/inici/Presentacio";
 
 const PaginaPrincipal = () => {
-	return (
-		<>
-			<MesDemanat />
-			<Qualitats />
-		</>
-	);
+  return (
+    <>
+      {/* <MesDemanat />
+			<Qualitats /> */}
+      <Presentacio />
+    </>
+  );
 };
 
 export default PaginaPrincipal;
