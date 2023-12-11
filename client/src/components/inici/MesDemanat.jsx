ed72e07a0fcea711bbc9c081fbea364806de25ba
marcdev30 <programarc30@gmail.com>
2023-12-11 15:20:14 +0100

    Arreglar problemes menors

--- client/src/components/inici/MesDemanat.jsx
+++ client/src/components/inici/MesDemanat.jsx
@@ -5,7 +5,7 @@ const MesDemanat = () => {
 				<h4>El més demanat</h4>
 				<h3>ENTREPÀ DE BACON</h3>
 				<p>L'entrepà de bacon és l'esmorzar que més ha triomfat l'últim mes.</p>
-				<a className="button" href="/">
+				<a className="button" href="/producte/6537d85e9d4f4863d3bf6355">
 					Reserva ara
 				</a>
 			</div>
